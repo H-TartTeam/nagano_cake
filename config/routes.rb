bf239d0ab72e1982a51bb70752c52c75ce77bfa7
marina-1205 <ec2-user@ip-172-31-30-171.ap-northeast-1.compute.internal>
2023-07-17 07:23:54 +0000

    Top・public/itemsを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :costomers
+  namespace :public do
+    root 'homes/top'
+    get "/about" => "homes#about"
+    resources :items, only: [:index, :show]
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_07_15_055508) do
+ActiveRecord::Schema.define(version: 2023_07_17_064750) do
 
-  create_table "users", force: :cascade do |t|
+  create_table "costomers", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -20,8 +20,25 @@ ActiveRecord::Schema.define(version: 2023_07_15_055508) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["email"], name: "index_costomers_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_costomers_on_reset_password_token", unique: true
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "introduction", null: false
+    t.integer "price", null: false
+    t.string "image_id"
+    t.integer "genre_id", null: false
+    t.boolean "is_available", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
