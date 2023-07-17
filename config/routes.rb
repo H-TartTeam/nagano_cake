7a45aae0cffdb07f92264940ad21c735e28c0c8a
EC2 Default User <ec2-user@ip-172-31-33-79.ap-northeast-1.compute.internal>
2023-07-17 11:16:08 +0000

    add config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,26 @@
 Rails.application.routes.draw do
 
-  　devise_for :admins
-  　devise_for :costomers
-  
- 　 namespace :public do
+    devise_for :admins
+    devise_for :costomers
+
+    namespace :public do
     get "/" => "homes#top"
     get 'homes/about'
- 　 end
-  
-　 　　namespace :public do
+    end
+
+    namespace :public do
     get 'items/show'
     get 'items/index'
-  　end
-  
- 　 namespace :public do
+    end
+
+    namespace :public do
     get 'orders/show'
     get 'orders/index'
     get 'orders/confirm'
     get 'orders/complete'
- 　 end
-  
- 　 namespace :public do
+    end
+
+    namespace :public do
     get 'cart_items/index'
-  　end
+    end
 end
