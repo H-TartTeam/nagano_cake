6250689b4df2e22c8fe975b5369c042f34809b5a
EC2 Default User <ec2-user@ip-172-31-9-210.ap-northeast-1.compute.internal>
2023-07-17 09:42:26 +0000

    [Fix] developをmerge

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,26 @@
 Rails.application.routes.draw do
 
-  　devise_for :admins
-  　devise_for :costomers
-  
- 　 namespace :public do
-    get "/" => "homes#top"
-    get 'homes/about'
- 　 end
-  
-　 　　namespace :public do
-    get 'items/show'
-    get 'items/index'
-  　end
-  
- 　 namespace :public do
-    get 'orders/show'
-    get 'orders/index'
-    get 'orders/confirm'
-    get 'orders/complete'
- 　 end
-  
- 　 namespace :public do
-    get 'cart_items/index'
-  　end
+  devise_for :admins
+  devise_for :costomers
+
+    namespace :public do
+      get "/" => "homes#top"
+      get 'homes/about'
+    end
+
+    namespace :public do
+      get 'items/show'
+      get 'items/index'
+    end
+
+    namespace :public do
+      get 'orders/show'
+      get 'orders/index'
+      get 'orders/confirm'
+      get 'orders/complete'
+    end
+
+    namespace :public do
+      get 'cart_items/index'
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,5 @@
-ActiveRecord::Schema.define(version: 2023_07_17_052841) do
-　　
+ActiveRecord::Schema.define(version: 2023_07_17_064750) do
+
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -10,6 +10,16 @@ ActiveRecord::Schema.define(version: 2023_07_17_052841) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "cart_items", force: :cascade do |t|
+    t.integer "quantity", default: 1, null: false
+    t.integer "customer_id", null: false
+    t.integer "item_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["customer_id"], name: "index_cart_items_on_customer_id"
+    t.index ["item_id"], name: "index_cart_items_on_item_id"
   end
 
   create_table "customers", force: :cascade do |t|
@@ -32,13 +42,6 @@ ActiveRecord::Schema.define(version: 2023_07_17_052841) do
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
 
-  create_table "shopping_addresses", force: :cascade do |t|
-    t.integer "customer_id", null: false
-    t.string "name", null: false
-    t.string "postcode", null: false
-    t.string "address", null: false
-  end
-
   create_table "genres", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
@@ -55,15 +58,17 @@ ActiveRecord::Schema.define(version: 2023_07_17_052841) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
-  
-  create_table "cart_items", force: :cascade do |t|
-    t.integer "quantity", default: 1, null: false
-    t.integer "customer_id", null: false
+
+  create_table "order_items", force: :cascade do |t|
     t.integer "item_id", null: false
+    t.integer "order_id", null: false
+    t.integer "price", null: false
+    t.integer "quantity", null: false
+    t.integer "make_status", default: 0, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["customer_id"], name: "index_cart_items_on_customer_id"
-    t.index ["item_id"], name: "index_cart_items_on_item_id"
+    t.index ["item_id"], name: "index_order_items_on_item_id"
+    t.index ["order_id"], name: "index_order_items_on_order_id"
   end
 
   create_table "orders", force: :cascade do |t|
@@ -79,17 +84,14 @@ ActiveRecord::Schema.define(version: 2023_07_17_052841) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["customer_id"], name: "index_orders_on_customer_id"
   end
-  
-  create_table "order_items", force: :cascade do |t|
-    t.integer "item_id", null: false
-    t.integer "order_id", null: false
-    t.integer "price", null: false
-    t.integer "quantity", null: false
-    t.integer "make_status", default: 0, null: false
+
+  create_table "shopping_addresses", force: :cascade do |t|
+    t.integer "customer_id", null: false
+    t.string "name", null: false
+    t.string "postcode", null: false
+    t.string "address", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["item_id"], name: "index_order_items_on_item_id"
-    t.index ["order_id"], name: "index_order_items_on_order_id"
   end
 
   add_foreign_key "cart_items", "customers"
