005e5a8969f900f47643111d4913e19dad2aa929
marina-1205 <ec2-user@ip-172-31-30-171.ap-northeast-1.compute.internal>
2023-07-17 07:49:29 +0000

    Top・public/itemsを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   devise_for :costomers
   namespace :public do
-    root 'homes/top'
-    get "/about" => "homes#about"
-    resources :items, only: [:index, :show]
+    get "/" => "homes#top"
+    get 'homes/about'
   end
-
+  namespace :public do
+    get 'items/show'
+    get 'items/index'
+  end
+ 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
