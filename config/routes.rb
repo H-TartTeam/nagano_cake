492179a978bec40827eeafab70e25effcab014f7
EC2 Default User <ec2-user@ip-172-31-33-79.ap-northeast-1.compute.internal>
2023-07-18 16:04:04 +0000

    Add 会員viewページ

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,20 @@
 Rails.application.routes.draw do
 
-  devise_for :admins
-  devise_for :customers
+devise_for :customers,skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+
+devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+}
 
   scope module: :public do
     root "homes#top"
     get "/about" => "homes#about"
     resources :items, only: [:index, :show,]
-    resources :orders, only: [:index, :show, :onfirm, :complete]
+    resources :customers, only: [:show, :edit, :update, :confirm_withdraw, :withdraw]
+    resources :orders, only: [:index, :show, :confirm, :complete]
     get 'cart_items/index'
   end
 end
-
