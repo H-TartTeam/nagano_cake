de4dd5425f4b5b940ee7d40798ba2562b0360899
EC2 Default User <ec2-user@ip-172-31-9-210.ap-northeast-1.compute.internal>
2023-07-18 03:09:35 +0000

    [Add] コメント

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,5 +1,6 @@
 class Public::ItemsController < ApplicationController
   def show
+  #商品をカートに入れられるようにする
   @Item = Item.find(params[:id])
   @cart_item = CartItem.new
   end
--- db/migrate/20230717022132_create_cart_items.rb
+++ db/migrate/20230717022132_create_cart_items.rb
@@ -2,8 +2,8 @@ class CreateCartItems < ActiveRecord::Migration[6.1]
   def change
     create_table :cart_items do |t|
 
-      #
       t.integer :quantity, null: false
+      #foreign_key: trueをFKカラムに使用すると自動的にIDを生成してくれるので便利
       t.references :customer, null: false, foreign_key: true
       t.references :item, null: false, foreign_key: true
 
