373f1622735ac7171579ab4b86a310dda3aeb1d0
EC2 Default User <ec2-user@ip-172-31-9-210.ap-northeast-1.compute.internal>
2023-07-20 12:10:36 +0000

    [Fnish] admin/index

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,16 +1,14 @@
 class Admin::CustomersController < ApplicationController
-  module Admin
-    def index
-      @customer = Customer.find(params[:id])
-      #.page(params[:page])ページネーション
-      @customers = Customers.all.page(params[:page])
-    end
-
-    def show
-    end
-
-    def edit
-    end
-
+  def index
+    #@customer = Customer.find(params[:id])
+    #page(params[:page])ページネーション
+    @customers = Customer.all.page(params[:page])
   end
+
+  def show
+  end
+
+  def edit
+  end
+
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -6,4 +6,8 @@ class Customer < ApplicationRecord
 
   has_many :cart_items, dependent: :destroy
 
+  def full_name
+    "#{last_name} #{first_name}"
+  end
+
 end
