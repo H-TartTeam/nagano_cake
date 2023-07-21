3ecd5f391606409d581ae1838d80a3db8726b18d
EC2 Default User <ec2-user@ip-172-31-9-210.ap-northeast-1.compute.internal>
2023-07-21 05:05:06 +0000

    [Fix] エラー修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,14 @@
 Rails.application.routes.draw do
 
-  devise_for :customers,skip: [:passwords], controllers: {
-
-
+  devise_for :customers, skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
 
-
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
-  sessions: "admin/sessions"
-}
+    sessions: "admin/sessions"
+  }
 
-    namespace :admin do
-    get "/" => "homes#top"
-    resources :customers, only: [:index, :show, :edit ]
-      
   namespace :admin do
     get "/" => "homes#top"
     resources :customers, only: [:index, :show, :edit, :update]
@@ -24,23 +17,13 @@ Rails.application.routes.draw do
   end
 
   scope module: :public do
-   root "homes#top"
+    root "homes#top"
     get "/about" => "homes#about"
-
-    resources :items, only: [:index, :show,]
-    resource :customers, only: [:new, :create, :edit, :update, :confirm_withdraw, :withdraw]
     get 'customers/mypage' => 'customers#show'
     get 'customers/infomation/edit' => 'customers#edit'
     patch '/customers/edit' => 'customers#update'
-    resources :orders, only: [:index, :show, :confirm, :complete]
-    #resources :cart_items, only: [:index, :create, :destroy] do
-      #member do
-        #カート内個数を変更する際に使用するパス
-        #patch 'item'
-        #patch 'customer'
-      #end
     resources :items, only: [:index, :show]
-    resources :customers, only: [:show, :edit, :update, :confirm_withdraw, :withdraw]
+    resources :customers, only: [:new, :create, :show, :edit, :update, :confirm_withdraw, :withdraw]
     resources :orders, only: [:new, :index, :show, :confirm, :complete]
     resources :cart_items, only: [:index, :update, :create, :destroy] do
       collection do
