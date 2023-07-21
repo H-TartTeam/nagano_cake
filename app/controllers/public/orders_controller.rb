029e5e1972af6f385cd88d0f8c507bc60b711879
EC2 Default User <ec2-user@ip-172-31-9-210.ap-northeast-1.compute.internal>
2023-07-21 12:38:55 +0000

    [Fnish] 7/21

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,16 +1,20 @@
 class Public::OrdersController < ApplicationController
 
   def new
+    # カートに商品が入っているかを確認
     cart_items = current_customer.cart_items
     if cart_items.present?
+      # カートが空でない場合、新しい注文オブジェクトを生成
       @order = Order.new
+      # 新しいお届け先のフォームに使う、新しい住所オブジェクトを生成
       @new_address = ShoppingAddress.new
+      # 登録済みのお届け先の一覧を取得し、フォームで選択するためのオブジェクトに割り当て
       @registered_addresses = current_customer.shopping_addresses
     else
+      # カートが空の場合、エラーメッセージを表示して前のページにリダイレクト
       flash[:notice] = "カートが空です"
       redirect_to request.referer
     end
   end
-
   # 他のアクションの定義...
 end
