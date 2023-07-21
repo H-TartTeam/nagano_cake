fcacc3edfc5823bbdb0517a32ad06c6b0bc8e9a5
EC2 Default User <ec2-user@ip-172-31-33-79.ap-northeast-1.compute.internal>
2023-07-21 09:00:51 +0000

    　退会機能追加

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -20,6 +20,15 @@ class Public::CustomersController < ApplicationController
     end
   end
 
+  def confirm_withdraw
+    @customer = Customer.find(current_customer.id)
+    # is_deletedカラムをtrueに変更することにより削除フラグを立てる
+    @customer.update(is_deleted: true)
+    reset_session
+    flash[:notice] = "退会処理を実行いたしました"
+    redirect_to root_path
+  end
+
 end
 
 
