77c8e910137205668440b81d7b876889b88cf106
EC2 Default User <ec2-user@ip-172-31-9-210.ap-northeast-1.compute.internal>
2023-07-21 01:21:01 +0000

    [Start] 7/21

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,6 +1,6 @@
 class Admin::CustomersController < ApplicationController
   def index
-    #@customer = Customer.find(params[:id])
+    #customer = Customer.find(params[:id])
     #page(params[:page])ページネーション
     @customers = Customer.all.page(params[:page])
   end
