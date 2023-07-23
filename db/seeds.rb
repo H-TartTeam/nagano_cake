af40628bcbea5602c7e351c419502ebac6353fa7
marina-1205 <ec2-user@ip-172-31-30-171.ap-northeast-1.compute.internal>
2023-07-23 06:31:22 +0000

    アプリケーションHTML追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,11 @@ Admin.create!(
   password: '202306'
 )
 
+
+#ジャンルカラム記述
+Genre.create(name: 'ケーキ')
+Genre.create(name: 'アニバーサリーケーキ')
+Genre.create(name: '焼き菓子')
+Genre.create(name: 'プリン')
+Genre.create(name: 'キャンディ')
+Genre.create(name: 'ドーナツ')
