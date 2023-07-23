1c3e8da7dda745c482a508ee83cc0da1549dd88b
EC2 Default User <ec2-user@ip-172-31-9-210.ap-northeast-1.compute.internal>
2023-07-23 00:35:03 +0000

    [Start] 7/23

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -18,28 +18,41 @@ class Public::OrdersController < ApplicationController
 
   # 注文情報確認画面を表示するアクション
   def confirm
-    #注文情報入力されたデータが入って本当に存在するかどうか
+    #注文情報入力されたデータが入って本当に存在するかどうか判断
     if params[:order]
+      #データが入る空の変数作成
       @order = Order.new(order_params)
+      #ログインしているcustomerデータが入る
       @order.customer_id = current_customer.id
+      #ログインしているcart_itemsデータが入る
       @cart_items = current_customer.cart_items
+      #カート内商品の合計金額を算出して入れる
       @total_amount = @cart_items.inject(0) { |sum, item| sum + item.subtotal }
+      #送料を入れる
       @order.postage = 800
+      #合計金額と送料を+して請求金額を算出して入れる
       @order.total_amount = @total_amount + @order.postage.to_i
 
       #配送先データが格納されて (ラジオボタンのenumの数字)
+         #自分の住所に配送を選んだ場合
       if params[:order][:select_address] == "0"
-        #自分の住所に配送を選んだ場合
+        #ログインしているユーザー自身の郵便番号が入る
         @order.postcode = current_customer.postcode
+        #ログインしているユーザー自身の住所が入る
         @order.address = current_customer.address
+        #ログインしているユーザーの氏と名が入る
         @order.name = current_customer.last_name + current_customer.first_name
-      #登録した配送先を選んだ場合
+             #登録した配送先を選んだ場合
       elsif params[:order][:select_address] == "1"
         #本当に登録されている配送先かどうか判断
         if ShoppingAddress.exists?(id: params[:order][:address_id])
+          #注文情報入力で選ばれた配送先のIDを入れている
           @address = ShoppingAddress.find(params[:order][:address_id])
+          #IDから名前を取り出して入れる
           @order.name = @address.name
-          @order.postcode = @address.postal_code
+          #IDから郵便番号を取り出して入れる
+          @order.postcode = @address.postalcode
+          #IDkら住所を取り出して入れる
           @order.address = @address.address
         else
           # 配送先情報が見つからない場合、エラーメッセージを表示して注文情報入力画面を再表示
@@ -48,8 +61,11 @@ class Public::OrdersController < ApplicationController
         end
       #新しい配送先を選んだ場合
       elsif params[:order][:select_address] == "2"
+        #注文情報入力画面のフォームに記述された名前データを入れる
         @order.name = params[:order][:name]
+        #上記と同じデータは郵便番号
         @order.postcode = params[:order][:postcode]
+
         @order.address = params[:order][:address]
       else
         # フォームの入力が不正な場合、注文情報入力画面を再表示
@@ -61,21 +77,29 @@ class Public::OrdersController < ApplicationController
   # 注文を作成するアクション
   def create
     @order = Order.new(order_params)
+    #ログインしているユーザーのcustomerデータを変数に入れる
     @order.customer_id = current_customer.id
+    #ログインしているユーザーのあcart_itemsデータを変数に入れる
     @cart_items = current_customer.cart_items
+    #カート内商品を受け取って合計金額を算出
     @total_amount = @cart_items.inject(0) { |sum, item| sum + item.subtotal }
+    #送料
     @order.postage = 800
+    #送料と合計金額を＋して請求金額を算出
     @order.total_amount = @total_amount + @order.postage.to_i
 
     if @order.save
       # 注文が保存された場合、注文商品情報も保存する
       current_customer.cart_items.each do |cart_item|
         @order_item = OrderItem.new
+        #cart_itemデータの中にあるitem_idを取り出して変数に格納
         @order_item.item_id = cart_item.item_id
+        #注文確定されたデータを
         @order_item.order_id = @order.id
-        @order_item.price_including_tax = cart_item.item_with_tax_price
+        @order_item.taxin_price = cart_item.price
         @order_item.quantity = cart_item.quantity
-        @order_item.production_status = 0
+        #注文ステータス
+        @order_item.status = 0
         @order_item.save
       end
       # 注文が完了したらカートを空にして、注文完了画面にリダイレクトする
