6891c290f415a6f338b7bff8c0e57b839c2b4a83
高橋亜友美 <kooszs12@gmail.com>
2023-07-24 09:47:57 +0000

    [Stop] rest

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,22 +1,28 @@
 class Admin::OrdersController < ApplicationController
-
+  # 注文詳細を表示するアクション
   def show
-    @order = Order.find(params[:id])
-    @order_items =  @order.order_items
+    @order = Order.find(params[:id]) # パラメーターで指定された注文の詳細情報を取得
+    @order_items = @order.order_items # 注文に紐づく注文商品の一覧を取得
   end
 
+  # 注文のステータスを更新するアクション
   def update
-   @order = Order.find(params[:id])
-   @order_items = @order.order_items
-   if @order.update(order_params)
-    @order_items.update_all(production_status: "waiting_for_production") if @order.status == "confirmation_of_payment"
-   end
-    redirect_to request.referer
+    @order = Order.find(params[:id]) # パラメーターで指定された注文の情報を取得
+    @order_items = @order.order_items # 注文に紐づく注文商品の一覧を取得
+
+    # 注文のステータスを更新
+    if @order.update(order_params)
+      # もし注文のステータスが 支払確認済み）であれば、紐づく注文商品の制作ステータスを 制作待ちに更新
+      @order_items.update_all(production_status: "waiting_for_production") if @order.status == "confirmation_of_payment"
+    end
+
+    redirect_to request.referer # 前のページにリダイレクトする
   end
 
   private
-   def order_params
-    params.require(:order).permit(:status)
-   end
 
+  # ストロングパラメーターの定義
+  def order_params
+    params.require(:order).permit(:status) # 注文のステータスを更新する際に許可するパラメーターを指定
+  end
 end
